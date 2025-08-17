f14bb70a4b421d484923c879f33d305664ca4e66
Alberto J Garcia <albertojg1991@gmail.com>
2025-08-17 14:14:53 -0700

    Update site 2025-08-17 14:14:53

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Sample Blog Post with LaTeX and Plots",
-        "excerpt":"This is a sample post converted from a Jupyter notebook.   The formula $x^2 + y^2 = z^2$ is Pythagoras’ theorem.   Block math example:   \\[\\int_0^\\infty e^{-x} dx = 1\\]  Here’s a sample plot:      ⬅ Back to Home  ","categories": ["blog"],
+        "excerpt":"This is a sample post converted from a Jupyter notebook.   The formula $x^2 + y^2 = z^2$ is Pythagoras’ theorem.   Block math example:   \\[\\int_0^\\infty e^{-x} dx = 1\\]  Here’s a sample plot:       ","categories": ["blog"],
         "tags": [],
         "url": "/blog/2025/08/17/sample-post.html",
         "teaser": null
