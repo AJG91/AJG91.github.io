a3582d4fddabc0ae24a36813be0b7ed0c09c3ae0
Alberto J Garcia <albertojg1991@gmail.com>
2025-08-17 12:34:49 -0700

    Fixed Latex issue.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Sample Post",
-        "excerpt":"  title: “Sample Blog Post with LaTeX and Plots” date: 2025-08-16 categories: [blog] —   This is a sample post converted from a Jupyter notebook.   Inline math example: $E=mc^2$   Block math example:   \\[\\int_0^\\infty e^{-x^2} dx = \\frac{\\sqrt{\\pi}}{2}\\]  Here’s a sample plot:     ","categories": [],
+        "excerpt":"  Title: “Sample Blog Post with LaTeX and Plots”  Date: 2025-08-16  Categories: [blog] —   This is a sample post converted from a Jupyter notebook.   Inline math example: $E=mc^2$   Block math example:   \\[\\int_0^\\infty e^{-x^2} dx = \\frac{\\sqrt{\\pi}}{2}\\]  Here’s a sample plot:     ","categories": [],
         "tags": [],
         "url": "/2025/08/16/sample-post.html",
         "teaser": null
