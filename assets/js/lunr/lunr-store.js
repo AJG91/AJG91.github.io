eee2a88fb5d8725fe7de1a1eb3143f08ce0c79b0
Alberto J Garcia <albertojg1991@gmail.com>
2025-08-23 12:56:31 -0700

    Update site 2025-08-23 12:56:31

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Sample Blog Post with LaTeX and Plots",
-        "excerpt":"This is a sample post converted from a Jupyter notebook. The formula $x^2 + y^2 = z^2$ is Pythagoras’ theorem. Block math example: \\[\\int_0^\\infty e^{-x} dx = 1\\] Here’s a sample plot: You can also add Python code: def sum(a,b): return a + b print(sum(2, 3)) Pretty cool right?! ⬅...","categories": ["blog"],
+        "title": "A review on projectile motion",
+        "excerpt":"With all the buzz surrounding AI and machine learning, it is not uncommon to hear the term ‘‘linear regression’’ being thrown around. But what is linear regression? Linear regression is a technique that can be used to model the relationship between a dependent quantity and an independent quantity. For example,...","categories": ["blog"],
         "tags": [],
-        "url": "/blog/2025/08/17/sample-post.html",
+        "url": "/blog/2025/08/16/projectile-motion.html",
+        "teaser": null
+      },{
+        "title": "Linear regression with Scikit-Learn: projectile motion",
+        "excerpt":"Last time, we went over projectile motion and how to code the equations up in Python. In this post, I will show how to apply linear regression in Python using Scikit-learn’s LinearRegression (https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html). The example we will be working with is a standard two-dimensional kinematics problem. We will have data...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2025/08/23/linear-regression-with-scikit-learn.html",
         "teaser": null
       },]
