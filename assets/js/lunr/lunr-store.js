ee780c305c32105ff59169b9c221fbc9576d1795
Alberto J Garcia <albertojg1991@gmail.com>
2025-08-23 13:12:51 -0700

    Update site 2025-08-23 13:12:51

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,12 +1,12 @@
 var store = [{
         "title": "A review on projectile motion",
-        "excerpt":"With all the buzz surrounding AI and machine learning, it is not uncommon to hear the term ‘‘linear regression’’ being thrown around. But what is linear regression? Linear regression is a technique that can be used to model the relationship between a dependent quantity and an independent quantity. For example,...","categories": ["blog"],
+        "excerpt":"August, 16, 2025 With all the buzz surrounding AI and machine learning, it is not uncommon to hear the term ‘‘linear regression’’ being thrown around. But what is linear regression? Linear regression is a technique that can be used to model the relationship between a dependent quantity and an independent...","categories": ["blog"],
         "tags": [],
         "url": "/blog/2025/08/16/projectile-motion.html",
         "teaser": null
       },{
         "title": "Linear regression with Scikit-Learn: projectile motion",
-        "excerpt":"Last time, we went over projectile motion and how to code the equations up in Python. In this post, I will show how to apply linear regression in Python using Scikit-learn’s LinearRegression. The example we will be working with is a standard two-dimensional kinematics problem. We will have data for...","categories": ["blog"],
+        "excerpt":"August, 23, 2025 Last time, we went over projectile motion and how to code the equations up in Python. In this post, I will show how to apply linear regression in Python using Scikit-learn’s LinearRegression. The example we will be working with is a standard two-dimensional kinematics problem. We will...","categories": ["blog"],
         "tags": [],
         "url": "/blog/2025/08/23/linear-regression-with-scikit-learn.html",
         "teaser": null
